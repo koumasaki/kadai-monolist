9b7dced7b3ba7d4e84f879e3494ca0f109e2d694
EC2 Default User <ec2-user@ip-172-31-6-172.us-east-2.compute.internal>
2018-07-23 14:17:05 +0000

    ranking

---
+++ app/Http/Controllers/RankingController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Item;
+
+class RankingController extends Controller
+{
+    public function want()
+    {
+//Wantの中間テーブルであるitem_userを指定。SQLでいうとFROM item_userに相当
+//$items = \DB::table('item_user')
+
+//items テーブルを item_user テーブルと結合しています。
+//->join('items', 'item_user.item_id', '=', 'items.id')
+
+//itemsテーブル全カラム、COUNT計算によって一時的に出現するカラムに、countという名前をつける
+//->select('items.*', \DB::raw('COUNT(*) as count'))
+
+//type = wantのみ
+//->where('type', 'want')
+
+//グループ化
+//->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')
+
+//countの多い順に並べる
+//->orderBy('count', 'DESC')
+
+//上位10件取得
+//->take(10)
+
+//データ取得
+//->get();
+
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+
+        return view('ranking.want', [
+            'items' => $items, 
+        ]);
+    }
+}
