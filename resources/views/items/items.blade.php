5f75313b9c4bafb0111afe90e6711683ae6b1ce7
EC2 Default User <ec2-user@ip-172-31-6-172.us-east-2.compute.internal>
2018-07-23 07:49:26 +0000

    adjust view

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -8,7 +8,11 @@
                         <img src="{{ $item->image_url }}" alt="{{ $item->name }}">
                     </div>
                     <div class="panel-body">
+                        @if($item->id)
                         <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                        @else
+                        <p class="item-title">{{ $item->name }}</p>
+                        @endif
                         <div class="buttons text-center">
                             @if (Auth::check())
                                 @include('items.want_button', ['item' => $item])
